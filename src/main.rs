bd730c3eb00d737599dc2e5998816e16f09c1f95
Arnold Tharrington <arnoldt100@hotmail.com>
2026-04-26 08:38:26 -0400

    Latest update for docs branch.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@
 
 mod command_line_options;
 use command_line_options::process_command_line_args;
+use command_line_options::_COMMAND_FILE_HELP_TEXT;
 
 mod setup_simulation;
 use setup_simulation::create_md_simulation;
@@ -19,9 +20,8 @@ mod cleanup_simulation;
 use cleanup_simulation::cleanup_md_simulation;
 
 fn main() {
-    // Process command line arguments. This will typically involve 
-    // parsing the arguments and storing them in a struct for later,
-    let options = process_command_line_args();
+    // Process command line arguments.
+    process_command_line_args();
     create_md_simulation();
     run_md_simulation();
     cleanup_md_simulation();
